e2b186709af59ab4939547327a3458d2e02751f3
Ayshika Kapoor <96912880+ayshikakap31@users.noreply.github.com>
2024-09-19 02:04:27 +0530

    Add files via upload

---
+++ Part_01/Module 5 Lesson 1/06_Training_models_in_sklearn.py
@@ -0,0 +1,35 @@
+import pandas
+import numpy
+
+# Read the data
+data = pandas.read_csv('data.csv')
+
+# Split the data into X and y
+X = numpy.array(data[['x1', 'x2']])
+y = numpy.array(data['y'])
+
+# import statements for the classification algorithms
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+
+# TODO: Pick an algorithm from the list:
+# - Logistic Regression
+# - Decision Trees
+# - Support Vector Machines
+# Define a classifier (bonus: Specify some parameters!)
+# and use it to fit the data, make sure you name the variable as "classifier"
+
+#To implement Logistic Regression
+classifier = LogisticRegression()
+classifier.fit(X,y)
+
+#To implement Decision Tree
+classifier = DecisionTreeClassifier()
+classifier.fit(X,y)
+
+#To implement Support Vector Machine
+classifier = SVC()
+classifier.fit(X,y)
+
+# Click on `Test Run` to see how your algorithm fit the data!
